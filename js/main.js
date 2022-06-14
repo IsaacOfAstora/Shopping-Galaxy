e6757e5f4ced47bfb90eadb296c58e9dae1e511e
Ishak <isaaco7274@gmail.com>
2022-06-14 21:46:11 +0100

    I swear changing drop down menu to be dynamic took me 5 hours

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("nav .button").click(function () {
+  $("nav .button span i").toggleClass("rotate");
+  $("nav .button .menu").toggleClass("dropDown");
+});
+
+/*________________________________________________________________ */
+
+$("nav .button-Products-html").click(function () {
+  $("nav .button-Products-html span i").toggleClass("rotate");
+  $("nav .button-Products-html .menu").toggleClass("dropDown");
+});
