ecd7b568ce3c793f727907b230ccb0439e7aa23c
Ishak <isaaco7274@gmail.com>
2022-06-25 13:57:17 +0100

    A lot of things

---
+++ js/for_custumer.js
@@ -0,0 +1,26 @@
+/*____________________Adding Cart Elements____________________*/
+
+/*____________________Adding End____________________*/
+
+/*<!-----!>*/
+/*<!-----!>*/
+/*<!-----!>*/
+
+/*<!-Removing a Cart element with profficional CSS animation-!>*/
+
+let cartItemRemover = document.getElementsByClassName("remover");
+
+for (let i = 0; i < cartItemRemover.length; i++) {
+  let removeButton = cartItemRemover[i];
+
+  removeButton.addEventListener("click", function (param) {
+    buttonRemoving =
+      param.target.parentElement.parentElement.parentElement.classList.add(
+        "vanquish"
+      );
+    +setTimeout(function () {
+      param.target.parentElement.parentElement.parentElement.remove();
+    }, 400);
+  });
+}
+/*<!-Removing a Cart element with profficional CSS animation End-!>*/
--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ $("nav .button-Products-html").click(function () {
   $("nav .button-Products-html span i").toggleClass("rotate");
   $("nav .button-Products-html .menu").toggleClass("dropDown");
 });
+
+const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const searchInput = document.querySelector(".input-box");
+searchBtn.onclick = () => {
+  searchBox.classList.add("active");
+  searchBtn.classList.add("active");
+  searchInput.classList.add("active");
+  cancelBtn.classList.add("active");
+  searchInput.focus();
+};
+cancelBtn.onclick = () => {
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("active");
+  searchInput.classList.remove("active");
+  cancelBtn.classList.remove("active");
+  searchInput.value = "";
+};
+/*________________________________________________________________ */
