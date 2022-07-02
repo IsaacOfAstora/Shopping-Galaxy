5a772c40f336b32f33245c6c7423468fd42894c2
Ishak <isaaco7274@gmail.com>
2022-07-02 16:08:22 +0100

    New Search bar made by me

--- js/main.js
+++ js/main.js
@@ -10,22 +10,21 @@ $("nav .button-Products-html").click(function () {
   $("nav .button-Products-html .menu").toggleClass("dropDown");
 });
 
-const searchBox = document.querySelector(".search-box");
-const searchBtn = document.querySelector(".search-icon");
-const cancelBtn = document.querySelector(".cancel-icon");
-const searchInput = document.querySelector(".input-box");
-searchBtn.onclick = () => {
-  searchBox.classList.add("active");
-  searchBtn.classList.add("active");
-  searchInput.classList.add("active");
-  cancelBtn.classList.add("active");
-  searchInput.focus();
+const searchBar = document.querySelector(".search-bar");
+const searchIcon = document.querySelector(".search-icon");
+const searchInput = document.querySelector(".search-input");
+const searchRemover = document.querySelector(".search-bar-remover");
+
+searchIcon.onclick = () => {
+  searchBar.classList.add("expanded");
+  searchIcon.classList.add("expanded");
+  searchInput.classList.add("expanded");
+  searchRemover.classList.add("expanded");
 };
-cancelBtn.onclick = () => {
-  searchBox.classList.remove("active");
-  searchBtn.classList.remove("active");
-  searchInput.classList.remove("active");
-  cancelBtn.classList.remove("active");
-  searchInput.value = "";
+searchRemover.onclick = () => {
+  searchBar.classList.remove("expanded");
+  searchIcon.classList.remove("expanded");
+  searchInput.classList.remove("expanded");
+  searchRemover.classList.remove("expanded");
 };
 /*________________________________________________________________ */
