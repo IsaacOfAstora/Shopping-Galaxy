2dad819e74ef49e774a1ca894e5acd7c7c5ce3db
Ishak <isaaco7274@gmail.com>
2022-07-04 16:16:58 +0100

    new home page prototype design

---
+++ js/home.js
@@ -0,0 +1,15 @@
+let signu_upu = document.querySelector(".sign-up-form");
+let logu_inu = document.querySelector(".log-in-form");
+let Already_Have = document.querySelector(".sign");
+let Dont_Have = document.querySelector(".log");
+
+signu_upu.classList.add("vanquish");
+
+Dont_Have.onclick = () => {
+  signu_upu.classList.remove("vanquish");
+  logu_inu.classList.add("vanquish");
+};
+Already_Have.onclick = () => {
+  signu_upu.classList.add("vanquish");
+  logu_inu.classList.remove("vanquish");
+};
