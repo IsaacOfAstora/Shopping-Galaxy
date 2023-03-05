4c65a8ea17898dcb903a641a681a9456ddeb274a
Isaac Touhami <isaaco7274@gmail.com>
2023-03-05 15:01:22 +0100

    The slider is perfect now  =)

--- js/announceColumn.js
+++ js/announceColumn.js
@@ -1,46 +1,61 @@
-let leftClick = document.querySelector(".icon-leftGalaxyShip")
-let rightClick = document.querySelector(".icon-rightGalaxyShip")
-let slider = document.querySelector(".slider-images")
-let sliderImage = document.querySelectorAll(".slider-images img")
-let trigger = document.querySelectorAll(".trigger span")
-
-
+let leftClick = document.querySelector(".icon-leftGalaxyShip");
+let rightClick = document.querySelector(".icon-rightGalaxyShip");
+let slider = document.querySelector(".slider-images");
+let sliderImage = document.querySelectorAll(".slider-images img");
+let trigger = document.querySelector(".trigger");
 
 let counter = 0;
-
 let size = sliderImage[0].clientWidth;
-trigger[counter].classList.add("colored")
 
+rightClick.addEventListener("click", () => {
+  if (counter >= sliderImage.length - 1) return;
+  slider.style.transition = "0.6s ease-in-out";
+  counter++;
+  slider.style.transform = "translateX(" + -size * counter + "px)";
+});
 
-rightClick.addEventListener("click", ()=>{
-    leftClick.classList.remove("colored")
-    if (counter >= sliderImage.length -1) return;
-    slider.style.transition= "0.6s ease-in-out";
-    counter++;
-    slider.style.transform = 'translateX('+ (-size * counter) + 'px)';
-    trigger[counter].classList.add("colored")
-    trigger[counter-1].classList.remove("colored")
-    if (counter == sliderImage.length -1) {
-        rightClick.classList.add("colored")
-    }
-})
+leftClick.addEventListener("click", () => {
+  if (counter <= 0) return;
+  slider.style.transition = "0.6s ease-in-out";
+  counter--;
+  slider.style.transform = "translateX(" + -size * counter + "px)";
+});
 
+slider.addEventListener("transitionend", () => {
+  if (sliderImage[counter].id === "lastClone") {
+    slider.style.transition = "none";
+    counter = sliderImage.length - 2;
+    slider.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (sliderImage[counter].id === "firstClone") {
+    slider.style.transition = "none";
+    counter = sliderImage.length - counter;
+    slider.style.transform = "translateX(" + -size * counter + "px)";
+  }
+});
 
-leftClick.addEventListener("click", ()=>{
-    rightClick.classList.remove("colored")
-    if (counter <= 0) return;
-    slider.style.transition= "0.6s ease-in-out";
-    counter--
-    slider.style.transform = 'translateX('+ (-size * counter) + 'px)';
-    trigger[counter+1].classList.remove("colored")
-    trigger[counter].classList.add("colored")
-    if (counter == 0) {
-        leftClick.classList.add("colored")
-    } 
-})
+for (let i = 0; i < sliderImage.length - 2; i++) {
+  let dot = document.createElement("span");
+  trigger.appendChild(dot);
+}
+let triggerDot = document.querySelectorAll(".trigger span");
+let i = 0;
+triggerDot[i].classList.add("colored");
 
+rightClick.onclick = () => {
+  triggerDot[i].classList.remove("colored");
+  i++;
+  if (i > 2) {
+    i = 0;
+  }
+  triggerDot[i].classList.add("colored");
+};
 
-    
-    
-
-
+leftClick.onclick = () => {
+  triggerDot[i].classList.remove("colored");
+  i--;
+  if (i < 0) {
+    i = 2;
+  }
+  triggerDot[i].classList.add("colored");
+};
