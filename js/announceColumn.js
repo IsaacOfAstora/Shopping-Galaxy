11e6b8d7573f495f92df37af3269401b789f4583
Isaac Touhami <isaaco7274@gmail.com>
2023-03-08 13:38:12 +0100

    dynamic slider

--- js/announceColumn.js
+++ js/announceColumn.js
@@ -9,58 +9,92 @@ let counter = 1;
 let size = sliderImage[0].clientWidth;
 slider.style.transform = "translateX(" + -size * counter + "px)";
 
+for (let i = 0; i < sliderImage.length - 2; i++) {
+  let dot = document.createElement("span");
+  trigger.appendChild(dot);
+}
+let triggerDot = document.querySelectorAll(".trigger span");
+
 rightClick.addEventListener("click", moveRight);
 function moveRight() {
   if (counter >= sliderImage.length - 1) return;
   slider.style.transition = "0.8s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
   counter++;
   slider.style.transform = "translateX(" + -size * counter + "px)";
-  triggerRight();
-}
 
-leftClick.addEventListener("click", () => {
+  triggerRight();
+  transitioned();
+}
+leftClick.addEventListener("click", moveLeft);
+function moveLeft() {
   if (counter <= 0) return;
   slider.style.transition = "0.8s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
   counter--;
   slider.style.transform = "translateX(" + -size * counter + "px)";
   triggerLeft();
-});
-
-slider.addEventListener("transitionend", () => {
-  if (sliderImage[counter].id === "lastClone") {
-    slider.style.transition = "none";
-    counter = sliderImage.length - 2;
-    slider.style.transform = "translateX(" + -size * counter + "px)";
-  }
-  if (sliderImage[counter].id === "firstClone") {
-    slider.style.transition = "none";
-    counter = sliderImage.length - counter;
-    slider.style.transform = "translateX(" + -size * counter + "px)";
-  }
-});
-
-for (let i = 0; i < sliderImage.length - 2; i++) {
-  let dot = document.createElement("span");
-  trigger.appendChild(dot);
+  transitioned();
 }
-let triggerDot = document.querySelectorAll(".trigger span");
+/**********____________________________________**********/
+/**********_____________autoDot______________************/
+/**********____________________________________**********/
+let x = 0;
+triggerDot[x].classList.add("colored");
+function triggerRight() {
+  triggerDot[x].style.transition = "0.5s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
+  triggerDot[x].classList.remove("colored");
+
+  x++;
+  if (x > 5) {
+    x = 0;
+  }
+  triggerDot[x].classList.add("colored");
+}
+
+function triggerLeft() {
+  triggerDot[x].style.transition = "0.5s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
+  triggerDot[x].classList.remove("colored");
+  x--;
+  if (x < 0) {
+    x = 5;
+  }
+  triggerDot[x].classList.add("colored");
+}
+/**********____________________________________**********/
+/********_____________autoDot End______________**********/
+/**********____________________________________**********/
 /**********____________________________________**********/
 /**********______________dotClick______________**********/
 /**********____________________________________**********/
-let j = 0;
+
 for (let i = 0; i < triggerDot.length; i++) {
   triggerDot[i].onclick = () => {
     triggerDot[i].style.transition = "0.5s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
+    triggerDot[x].classList.remove("colored");
     triggerDot[i].classList.add("colored");
-    triggerDot[j].classList.remove("colored");
-    j = i;
     slider.style.transition = "0.8s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
-    slider.style.transform = "translateX(" + -size * i + "px)";
+    slider.style.transform = "translateX(" + -size * (i + 1) + "px)";
+    x = i;
+    counter = i + 1;
   };
 }
 /**********____________________________________**********/
 /********______________dotClick End______________********/
 /**********____________________________________**********/
+
+function transitioned() {
+  slider.addEventListener("transitionend", () => {
+    if (sliderImage[counter].id === "lastClone") {
+      slider.style.transition = "none";
+      counter = sliderImage.length - 2;
+      slider.style.transform = "translateX(" + -size * counter + "px)";
+    }
+    if (sliderImage[counter].id === "firstClone") {
+      slider.style.transition = "none";
+      counter = sliderImage.length - counter;
+      slider.style.transform = "translateX(" + -size * counter + "px)";
+    }
+  });
+}
 
 /**********____________________________________**********/
 /**********______________Autoplay______________**********/
@@ -86,43 +120,3 @@ mainSlider.addEventListener("mouseout", autoplay);
 /**********____________________________________**********/
 /**********___________playstate End____________**********/
 /**********____________________________________**********/
-
-// function letTrigger() {
-//   for (let i = 0; i < triggerDot.length; i++) {
-//     triggerDot[i].classList.remove("colored");
-//   }
-//   triggerDot[counter].classList.add("colored");
-// }
-let i = 0;
-triggerDot[i].classList.add("colored");
-function triggerRight() {
-  triggerDot[i].style.transition = "0.5s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
-  triggerDot[i].classList.remove("colored");
-  i++;
-  if (i > 5) {
-    i = 0;
-  }
-  triggerDot[i].classList.add("colored");
-}
-
-function triggerLeft() {
-  triggerDot[i].style.transition = "0.5s cubic-bezier(0.9,-0.04, 0.71, 1.05)";
-  triggerDot[i].classList.remove("colored");
-  i--;
-  if (i < 0) {
-    i = 5;
-  }
-  triggerDot[i].classList.add("colored");
-}
-
-/********************************************************/
-/********************************************************/
-/********************************************************/
-/********************************************************/
-/*******************______________***********************/
-/********************Second Slider***********************/
-/*******************______________***********************/
-/********************************************************/
-/********************************************************/
-/********************************************************/
-/********************************************************/
